8766996da7539d27d9113383bb25f6f3905f1f77
Andrew Midthun <midth002@gmail.com>
2022-05-08 18:23:44 -0500

    Added logout functionality once a user is logged in on the home page. Made a separate logout route file in the api folder. Also a logout.js file in the public/js folder location.

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,10 +1,12 @@
 const router = require('express').Router();
 const loginRoute = require('./loginRoute');
+const logoutRoute = require('./logoutRoute');
 // const postRoute = require('./postRoute');
 const signupRoute = require('./signupRoute');
 
 
 router.use('/login', loginRoute);
+router.use('/logout', logoutRoute);
 // router.use('/post', postRoute);
 router.use('/signup', signupRoute);
 
