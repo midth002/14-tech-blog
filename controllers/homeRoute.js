3c403c0a445ab44beed281e76b74c0c89c07905e
Andrew Midthun <midth002@gmail.com>
2022-05-08 16:17:32 -0500

    Added main and home handlebar html. Added get route in the home route.

--- controllers/homeRoute.js
+++ controllers/homeRoute.js
@@ -1,23 +1,17 @@
 const router = require('express').Router();
-const { User } = require('../models');
+const { User, Post, Comment } = require('../models');
+const auth = require('../utils/auth'); 
 
 router.get('/', async (req, res) => {
-  try {
-    // Get all users, sorted by name
-    const userData = await User.findAll({
-      attributes: { exclude: ['password'] },
-      order: [['name', 'ASC']],
-    });
+    try {
+        res.render('home')
+    } catch (err) {
+        console.log(err); 
+        res.status(500).json(err);
+    }
+    
+})
 
-    // Serialize user data so templates can read it
-    const users = userData.map((project) => project.get({ plain: true }));
-
-    // Pass serialized data into Handlebars.js template
-    res.render('homepage', { users });
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
 
 module.exports = router;
 
