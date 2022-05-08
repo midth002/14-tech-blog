ebf214b1283427c55cf9a7d437dc1c0d743b4a56
Andrew Midthun <midth002@gmail.com>
2022-05-08 17:22:15 -0500

    Added auth helper function for redirecting to the login page.

--- controllers/homeRoute.js
+++ controllers/homeRoute.js
@@ -2,9 +2,11 @@ const router = require('express').Router();
 const { User, Post, Comment } = require('../models');
 const auth = require('../utils/auth'); 
 
-router.get('/', async (req, res) => {
+router.get('/', auth, async (req, res) => {
     try {
-        res.render('home')
+        res.render('home', {
+
+        })
     } catch (err) {
         console.log(err); 
         res.status(500).json(err);
@@ -12,7 +14,25 @@ router.get('/', async (req, res) => {
     
 });
 
+router.get('/login', (req, res) => {
+    // If a session exists, redirect the request to the homepage
+    if (req.session.logged_in) {
+      res.redirect('/');
+      return;
+    }
+     res.render('login');
+  });
 
+  router.get('/signup',  (req, res)=> {
+    try {
+  
+      res.render('signup');
+      
+    } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+    }
+  });
 
 
 
